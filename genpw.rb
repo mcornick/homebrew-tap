e8945b06ef7646c09a6ead20191f5443296545d6
Mark Cornick <mark@markcornick.com>
2021-10-21 22:16:26 -0400

    Brew formula update for genpw version v4.2.3

--- genpw.rb
+++ genpw.rb
@@ -5,29 +5,41 @@
 class Genpw < Formula
   desc "Generates strings that can be used as reasonably secure passwords."
   homepage "https://github.com/markcornick/genpw"
-  version "4.2.2"
+  version "4.2.3"
 
   on_macos do
-    url "https://github.com/markcornick/genpw/releases/download/v4.2.2/genpw_4.2.2_darwin_all.tar.gz"
-    sha256 "04d235ffc6af89e9c9abf048f9bd12f2ab5b6850d9ce876b0b30ccbaa0df01f7"
+    url "https://github.com/markcornick/genpw/releases/download/v4.2.3/genpw_4.2.3_darwin_all.tar.gz"
+    sha256 "3002db2f8fd9ad1a061e4de7dedf82e0ecba93e6c6f51537cad1d15f412d3dd0"
+
+    def install
+      bin.install "genpw"
+    end
   end
 
   on_linux do
     if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-      url "https://github.com/markcornick/genpw/releases/download/v4.2.2/genpw_4.2.2_linux_armv6.tar.gz"
-      sha256 "e41cb644244a4a64d8b8b319c3c01b78f919e78897735ef83a6459393cad2c6f"
-    end
-    if Hardware::CPU.intel?
-      url "https://github.com/markcornick/genpw/releases/download/v4.2.2/genpw_4.2.2_linux_amd64.tar.gz"
-      sha256 "ea7319ab6f0c1925bdd9befc21fd4fdc6aa91dae8d0319175549c561b0131e81"
+      url "https://github.com/markcornick/genpw/releases/download/v4.2.3/genpw_4.2.3_linux_armv6.tar.gz"
+      sha256 "cf2146f367b0785b90c20ab466a3740247db7dc70ade7d89652ac4adcc84976f"
+
+      def install
+        bin.install "genpw"
+      end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/markcornick/genpw/releases/download/v4.2.2/genpw_4.2.2_linux_arm64.tar.gz"
-      sha256 "5f9911a984f8ed16edadb7248473d6136d8f5597dd1d3344fd40feeaae05d98b"
-    end
-  end
+      url "https://github.com/markcornick/genpw/releases/download/v4.2.3/genpw_4.2.3_linux_arm64.tar.gz"
+      sha256 "f8f8182bc1604f7858e925e15fdb38ce3e9123532c463b3ed32c7fa667f05b52"
 
-  def install
-    bin.install "genpw"
+      def install
+        bin.install "genpw"
+      end
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/markcornick/genpw/releases/download/v4.2.3/genpw_4.2.3_linux_amd64.tar.gz"
+      sha256 "74378a538f8f7de863b1e4232c55a84f441ab2640019598bfac132d922aa29ee"
+
+      def install
+        bin.install "genpw"
+      end
+    end
   end
 end
